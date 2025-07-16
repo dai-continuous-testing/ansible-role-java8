d654094aa6d12d4d5415cd101f16be6ee47b41be
Omprakash Paliwal <omprakash.paliwal@digital.ai>
2025-07-16 10:50:42 +0000

    Revert "fix java reinstall on version mismatch"
    This reverts commit 1863b5fc9a0adccafdbeeeaeeb62c75056fb9787.

--- filter_plugins/os_name_filter.py
+++ filter_plugins/os_name_filter.py
@@ -4,8 +4,7 @@
 class FilterModule(object):
     def filters(self):
         return {
-            'os_family_to_os_type': self.os_family_to_os_type,
-            'ansible_arch_to_java_arch': self.ansible_arch_to_java_arch
+            'os_family_to_os_type': self.os_family_to_os_type
         }
 
     def os_family_to_os_type(self, os_family):
@@ -19,21 +18,3 @@ class FilterModule(object):
         })                   
 
         return os_family_to_os_type_dict[os_family.lower()]
-
-    def ansible_arch_to_java_arch(self, ansible_arch):
-        """
-        Convert Ansible architecture names to Java download architecture names
-        """
-        arch_mapping = {
-            # Intel/AMD 64-bit
-            "x86_64": "x64",
-            "amd64": "x64",
-            # ARM 64-bit (Apple Silicon, etc.)
-            "arm64": "aarch64", 
-            "aarch64": "aarch64",
-            # Intel 32-bit (legacy)
-            "i386": "x32",
-            "i686": "x32"
-        }
-        
-        return arch_mapping.get(ansible_arch.lower(), ansible_arch)
